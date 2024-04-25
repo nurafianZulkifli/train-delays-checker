e8fcf6414b7c7b175ea9343e28f4068dbc53ae83
_nrfz <63917225+nurafianZulkifli@users.noreply.github.com>
2024-04-25 21:40:22 +0800

    2024-04-25

--- assets/js/charts-sbst/2024.js
+++ assets/js/charts-sbst/2024.js
@@ -23,7 +23,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(193, 205, 236, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [2,0,0,0],
+      data: [2,0,0,1],
     }],
   },
   options: {

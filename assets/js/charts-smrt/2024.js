eb21ba82c9c9ed208d8958b00dcb9f07a12c1edb
_nrfz <63917225+nurafianZulkifli@users.noreply.github.com>
2024-05-11 19:14:27 +0800

    2024-05-11

--- assets/js/charts-smrt/2024.js
+++ assets/js/charts-smrt/2024.js
@@ -23,7 +23,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(193, 205, 236, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [1,1,2,0],
+      data: [1,1,2,1,0],
     }],
   },
   options: {

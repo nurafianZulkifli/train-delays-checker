4931a673b58f3c9d7ae65f35b0c6258d9b88064e
nurafianZulkifli <nurafian.bz33@gmail.com>
2024-08-21 19:47:45 +0800

    2024-09 Feature Update to Version 2409

---
+++ js/charts-sbst/2023.js
@@ -0,0 +1,85 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Poppins';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+
+
+// Area Chart Example
+var ctx = document.getElementById("delays-overview-2023");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+      label: "Number of Delays",
+      lineTension: 0.3,
+      backgroundColor: "rgba(130, 71, 148,0.05)",
+      borderColor: "rgb(130, 71, 148)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgb(80, 44, 98)",
+      pointBorderColor: "rgb(130, 71, 148)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgb(130, 71, 148)",
+      pointHoverBorderColor: "rgb(130, 71, 148)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: [0,1,0,0,0,0,0,0,0],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 12,
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          color: "rgb(224, 225, 220)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        },
+        ticks: {
+          stepSize: 1,
+          beginAtZero: true,
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+    }
+  }
+});

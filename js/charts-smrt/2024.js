a02acd9abedcd447295478c3e5f26f507025f6e4
nrfz <nurafian.bz33@gmail.com>
2024-12-15 22:30:18 +0800

    2024-12 Data Update 1512

--- js/charts-smrt/2024.js
+++ js/charts-smrt/2024.js
@@ -7,7 +7,7 @@ const doverviewsm24 = new Chart(doverview24sm, {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
       label: "No. of Delays",
-      data: [1,1,2,1,1,1,2,2,9,3,0,0],
+      data: [1,1,2,1,1,1,2,2,9,3,0,1],
       borderColor: 'rgb(207, 61, 61)',
       fill: true,
       backgroundColor: 'rgba(207, 61, 61,0.05)',
